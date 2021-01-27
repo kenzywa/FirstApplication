a1ba661ff7aaa77568c66af4b5c958303ca82757
Shukailo Roman <shukailo99@gmail.com>
2021-01-27 23:27:20 +0300

    Initial Commit

---
+++ FirstApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstApplication
+//
+//  Created by Roman Shukailo on 27.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
